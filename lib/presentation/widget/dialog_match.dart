36bc0ecfd23bed4bbf80066a8c973d61f5219579
JamesTammila <tammilajames@gmail.com>
2023-01-28 13:53:28 +0100

    Stuff

--- lib/presentation/widget/dialog_match.dart
+++ lib/presentation/widget/dialog_match.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:didit/domain/model/model_user.dart';
+import 'package:didit/presentation/widget/view_user.dart';
 
 class MatchDialog extends StatelessWidget {
   const MatchDialog({super.key});
@@ -6,20 +8,93 @@ class MatchDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text("Tell James what to post next."),
-      content: TextFormField(
-        maxLength: 100,
-        maxLines: 5,
-        decoration: const InputDecoration(border: InputBorder.none),
-        onChanged: (s) => {},
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text("Shoe Selfie"),
+          Text("22:10"),
+        ],
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              UserView(
+                userModel: UserModel(
+                  objectId: '',
+                  createdAt: '',
+                  username: 'You',
+                  proPicUri:
+                      'https://qph.cf2.quoracdn.net/main-qimg-11d0c7a027d67e01bfd550dc0f0237da-lq',
+                  friendState: 'ME',
+                  requestId: '',
+                ),
+              ),
+              Icon(Icons.access_time_filled_rounded, color: Colors.yellow),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              UserView(
+                userModel: UserModel(
+                  objectId: '',
+                  createdAt: '',
+                  username: 'Anna',
+                  proPicUri:
+                  'https://data.whicdn.com/images/298487925/original.jpg?t=1534274746',
+                  friendState: 'ME',
+                  requestId: '',
+                ),
+              ),
+              Icon(Icons.access_time_filled_rounded, color: Colors.yellow),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              UserView(
+                userModel: UserModel(
+                  objectId: '',
+                  createdAt: '',
+                  username: 'Johanna',
+                  proPicUri:
+                  'https://i.pinimg.com/736x/a3/f6/91/a3f691a2c699fb79da00aace939b267b.jpg',
+                  friendState: 'ME',
+                  requestId: '',
+                ),
+              ),
+              Icon(Icons.check, color: Colors.green),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              UserView(
+                userModel: UserModel(
+                  objectId: '',
+                  createdAt: '',
+                  username: 'Adam',
+                  proPicUri:
+                  'https://i.pinimg.com/736x/e0/21/8f/e0218f2b6a1d7c71f080749045233c63.jpg',
+                  friendState: 'ME',
+                  requestId: '',
+                ),
+              ),
+              Icon(Icons.check, color: Colors.green),
+            ],
+          ),
+        ],
       ),
       actions: <Widget>[
-        ElevatedButton(
-          child: const Text("Cancel"),
+        TextButton(
+          child: const Text("Leave", style: TextStyle(color: Colors.red)),
           onPressed: () => Navigator.pop(context),
         ),
-        ElevatedButton(
-          child: const Text("Send"),
+        FilledButton(
+          child: const Text("Post"),
           onPressed: () => {},
         ),
       ],
--- lib/presentation/widget/view_match.dart
+++ lib/presentation/widget/view_match.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:didit/domain/model/model_match.dart';
-import 'package:didit/presentation/widget/view_picture.dart';
+import 'package:didit/presentation/widget/view_picture_small.dart';
 
 class MatchView extends StatelessWidget {
   const MatchView({super.key, required this.matchModel});
@@ -13,30 +13,33 @@ class MatchView extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(matchModel.theme),
-            Row(
-              children: [
-                PictureView(uri: matchModel.posts[0].user.proPicUri),
-                const SizedBox(width: 5),
-                PictureView(uri: matchModel.posts[1].user.proPicUri),
-                const SizedBox(width: 5),
-                PictureView(uri: matchModel.posts[2].user.proPicUri),
-                const SizedBox(width: 5),
-                PictureView(uri: matchModel.posts[3].user.proPicUri),
-              ],
-            ),
-          ],
+        Padding(
+          padding: const EdgeInsets.only(left: 5, right: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(matchModel.theme),
+              Row(
+                children: [
+                  SmallPictureView(uri: matchModel.posts[0].user.proPicUri),
+                  const SizedBox(width: 5),
+                  SmallPictureView(uri: matchModel.posts[1].user.proPicUri),
+                  const SizedBox(width: 5),
+                  SmallPictureView(uri: matchModel.posts[2].user.proPicUri),
+                  const SizedBox(width: 5),
+                  SmallPictureView(uri: matchModel.posts[3].user.proPicUri),
+                ],
+              ),
+            ],
+          ),
         ),
         const SizedBox(height: 10),
         Card(
           child: AspectRatio(
-            aspectRatio: 3 / 4,
+            aspectRatio: 4 / 5,
             child: GridView.count(
               primary: false,
-              childAspectRatio: 3 / 4,
+              childAspectRatio: 4 / 5,
               crossAxisCount: 2,
               crossAxisSpacing: 1,
               mainAxisSpacing: 1,
@@ -66,43 +69,6 @@ class MatchView extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 25),
-        /*Stack(
-          children: [
-            Card(
-              child: InteractiveViewer(
-                panEnabled: false,
-                child: AspectRatio(
-                  aspectRatio: 4 / 5,
-                  child: CachedNetworkImage(
-                    fit: BoxFit.cover,
-                    imageUrl: matchModel.posts[0].mediaUri,
-                    cacheKey:
-                        matchModel.posts[0].mediaUri.toString().split('?')[0],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 5,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  UserView(userModel: matchModel.posts[0].user),
-                  const SizedBox(width: 5),
-                  PopupMenuButton(
-                    icon: const Icon(Icons.more_vert),
-                    itemBuilder: (context) => <PopupMenuEntry>[
-                      const PopupMenuItem(child: Text('Report Post')),
-                      const PopupMenuItem(child: Text('Block User')),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),*/
       ],
     );
   }
