afbcb94e47b952b4acc037ae479df66046fec9ad
JamesTammila <tammilajames@gmail.com>
2023-01-28 16:55:27 +0100

    Auth

--- lib/domain/bloc/cubit_page_auth.dart
+++ lib/domain/bloc/cubit_page_auth.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 class AuthPageCubit extends Cubit<AuthPageState> {
   AuthPageCubit() : super(Initial());
@@ -25,6 +26,20 @@ class AuthPageCubit extends Cubit<AuthPageState> {
         },
         codeSent: (String verificationId, int? resendToken) {
           debugPrint("CS: $verificationId $resendToken");
+
+          /*final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: [
+            'email',
+            'https://www.googleapis.com/auth/contacts.readonly'
+          ]);
+
+          signInGoogle() async {
+            GoogleSignInAccount account = await _googleSignIn.signIn();
+            GoogleSignInAuthentication authentication =
+                await account.authentication;
+            await ParseUser.loginWith(
+                'google',
+                google(authentication.accessToken!, _googleSignIn.currentUser!.id, authentication.idToken!));
+          }*/
         },
         codeAutoRetrievalTimeout: (String verificationId) {
           debugPrint("CS: $verificationId");
