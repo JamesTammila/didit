a5f7ec92f471d442f5eef24dd27c6224f0bb75e6
jamestammila <tammilajames@gmail.com>
2023-03-01 10:17:52 +0100

    Observer

--- lib/observer.dart
+++ lib/observer.dart
@@ -7,4 +7,10 @@ class CubitObserver extends BlocObserver {
     super.onChange(bloc, change);
     debugPrint('${bloc.runtimeType} $change');
   }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    debugPrint('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
 }
