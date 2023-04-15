b371ac979b8ab7fac4e7c8cc76c0c2b04ec89235
jamestammila <tammilajames@gmail.com>
2023-04-15 22:58:00 +0200

    Search Cubit

--- lib/feature/search/bloc/cubit_search.dart
+++ lib/feature/search/bloc/cubit_search.dart
@@ -5,7 +5,7 @@ import 'package:didit/repo/repo_user.dart';
 import 'package:didit/model/model_user.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit(this.userRepository) : super(SearchLoading()) {
+  SearchCubit(this.userRepository) : super(SearchInit()) {
     subscription = userRepository.searchStream.listen(
       (users) {
         if (users.isEmpty) {
@@ -23,39 +23,25 @@ class SearchCubit extends Cubit<SearchState> {
   late final StreamSubscription subscription;
   Timer? timer;
 
-  void init() async {
-    try {
-      final Map<String, UserModel> recent = await userRepository.getRecent();
-      emit(SearchRecent(recent));
-    } on String catch (error) {
-      emit(SearchError(error));
-    }
-  }
+  void init() async => subscription.pause();
 
   void fetchSearch(String searchInput) async {
-    timer?.cancel();
     try {
-      if (searchInput.isEmpty) {
-        if (!subscription.isPaused) subscription.pause();
-        final Map<String, UserModel> recent = await userRepository.getRecent();
-        emit(SearchRecent(recent));
-      } else {
+      timer?.cancel();
+      if (searchInput.length > 2) {
         if (subscription.isPaused) subscription.resume();
         timer = Timer(const Duration(milliseconds: 500), () async {
           if (state is! SearchLoading) emit(SearchLoading());
           await userRepository.getSearch(searchInput);
         });
+      } else {
+        if (!subscription.isPaused) subscription.pause();
+        emit(SearchInit());
       }
     } on String catch (error) {
       emit(SearchError(error));
     }
   }
-
-  void addRecent(UserModel userModel) async =>
-      await userRepository.insertRecent(userModel);
-
-  void removeRecent(UserModel userModel) async =>
-      await userRepository.removeRecent(userModel);
 
   @override
   Future<void> close() async {
@@ -67,18 +53,14 @@ class SearchCubit extends Cubit<SearchState> {
 @immutable
 abstract class SearchState {}
 
+class SearchInit extends SearchState {}
+
 class SearchLoading extends SearchState {}
 
 class SearchLoaded extends SearchState {
   final Map<String, UserModel> search;
 
   SearchLoaded(this.search);
-}
-
-class SearchRecent extends SearchState {
-  final Map<String, UserModel> recent;
-
-  SearchRecent(this.recent);
 }
 
 class SearchEmpty extends SearchState {}
