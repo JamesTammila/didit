8f6a40a8f523430a4a87fa47edf0cd4b4763d8fa
jamestammila <tammilajames@gmail.com>
2023-04-24 08:10:25 +0200

    Clean Up

--- lib/feature/user/widget/button_user_action.dart
+++ lib/feature/user/widget/button_user_action.dart
@@ -3,8 +3,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:didit/feature/user/bloc/cubit_user.dart';
 import 'package:didit/common/dialog_error.dart';
 
-class UserActionButton extends StatelessWidget {
+class UserActionButton extends StatefulWidget {
   const UserActionButton({super.key});
+
+  @override
+  UserActionButtonState createState() => UserActionButtonState();
+}
+
+class UserActionButtonState extends State<UserActionButton> with TickerProviderStateMixin {
+  late final AnimationController controller = AnimationController(
+    duration: const Duration(milliseconds: 250),
+    vsync: this,
+  )..forward(from: 0);
+  late final Animation<double> animation = CurvedAnimation(
+    parent: controller,
+    curve: Curves.easeIn,
+  );
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(context) {
@@ -33,46 +53,55 @@ class UserActionButton extends StatelessWidget {
       },
       builder: (BuildContext context, state) {
         if (state is UserPending) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Card(
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text('Pending'),
+          return FadeTransition(
+            opacity: animation,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Card(
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text('Pending'),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              TextButton(
-                onPressed: () => context.read<UserCubit>().cancelRequest(),
-                child: const Text('Cancel Request'),
-              ),
-            ],
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () => context.read<UserCubit>().cancelRequest(),
+                  child: const Text('Cancel Request'),
+                ),
+              ],
+            ),
           );
         } else if (state is UserWaiting) {
-          return Row(
-            children: [
-              Expanded(
-                child: FilledButton(
-                  onPressed: () => context.read<UserCubit>().acceptRequest(),
-                  child: const Text('Accept'),
+          return FadeTransition(
+            opacity: animation,
+            child: Row(
+              children: [
+                Expanded(
+                  child: FilledButton(
+                    onPressed: () => context.read<UserCubit>().acceptRequest(),
+                    child: const Text('Accept'),
+                  ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: OutlinedButton(
-                  onPressed: () => context.read<UserCubit>().rejectRequest(),
-                  child: const Text('Reject'),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () => context.read<UserCubit>().rejectRequest(),
+                    child: const Text('Reject'),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         } else if (state is UserRandom) {
-          return FilledButton(
-            onPressed: () => context.read<UserCubit>().sendRequest(),
-            child: const Text('Add Friend'),
+          return FadeTransition(
+            opacity: animation,
+            child: FilledButton(
+              onPressed: () => context.read<UserCubit>().sendRequest(),
+              child: const Text('Add Friend'),
+            ),
           );
         } else if (state is UserLoadingError) {
           return Container(
