f049505ebbaada45ca1afdb2756bb2d9461d4211
jamestammila <tammilajames@gmail.com>
2023-04-24 08:11:30 +0200

    env File Attempt 1

--- lib/util/options_firebase.dart
+++ lib/util/options_firebase.dart
@@ -52,3 +52,58 @@ class DefaultFirebaseOptions {
     }
   }
 }
+
+/*import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+            'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return FirebaseOptions(
+          apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID']!,
+          appId: dotenv.env['FIREBASE_APP_ID_ANDROID']!,
+          messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_ANDROID']!,
+          projectId: dotenv.env['FIREBASE_PROJECT_ID_ANDROID']!,
+          storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_ANDROID']!,
+        );
+      case TargetPlatform.iOS:
+        return FirebaseOptions(
+          apiKey: dotenv.env['FIREBASE_API_KEY_IOS']!,
+          appId: dotenv.env['FIREBASE_APP_ID_IOS']!,
+          messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_IOS']!,
+          projectId: dotenv.env['FIREBASE_PROJECT_ID_IOS']!,
+          storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_IOS']!,
+          iosClientId: dotenv.env['FIREBASE_IOS_CLIENT_ID']!,
+          iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macOS - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for Windows - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for Linux - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+}*/
--- lib/util/options_parse.dart
+++ lib/util/options_parse.dart
@@ -6,3 +6,14 @@ class DefaultParseOptions {
   static const bool debug = false;
   //static const bool debug = true;
 }
+
+/*import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class DefaultParseOptions {
+  static const String appName = 'Jumbl';
+  static String serverUrl = dotenv.env['SERVER_URL']!;
+  //static const String serverUrl = 'http://192.168.50.252:1337/';
+  static String clientKey = dotenv.env['CLIENT_KEY']!;
+  static const bool debug = false;
+//static const bool debug = true;
+}*/
